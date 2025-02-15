c0b2a04ed85b4c7845e6ce14bb1d00cc412fb7eb
Christian <chrisdaq@gmail.com>
2025-02-15 12:47:16 -0400

    add express-validator package, set check middleware on route definition, read errors on controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,26 @@
+const { validationResult } = require("express-validator");
+
 const userAdd = (req, res) => {
-  res.json({
+  // get params from req.body
+  const { name, email, password } = req.body;
+
+  // validation errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      // errors serialized
+      errors: errors.mapped(),
+    });
+  }
+
+  res.status(201).json({
     ok: true,
   });
 };
 
 const userLogin = (req, res) => {
+  const { email, password } = req.body;
   res.json({
     ok: true,
   });
--- routes/auth.js
+++ routes/auth.js
@@ -5,8 +5,20 @@
 const express = require("express");
 const router = express.Router();
 const { userAdd, userLogin, tokenRenew } = require("../controllers/auth");
+const { check } = require("express-validator");
 
-router.post("/new", userAdd);
+router.post(
+  "/new",
+  [
+    // middlewares
+    check("name", "name is required").not().isEmpty(),
+    check("email", "email is required").isEmail(),
+    check("password", "password is required abd must have 6 characters length")
+      .not()
+      .isLength({ min: 6 }),
+  ],
+  userAdd
+);
 
 router.post("/", userLogin);
 
