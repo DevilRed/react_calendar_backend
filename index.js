1750face33eb666f772ae4743b8db51fc3ce724e
Christian <chrisdaq@gmail.com>
2025-02-16 15:52:23 -0400

    add mongoose, write db config, import it on app index

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 require("dotenv").config();
+const { dbConnection } = require("./database/config");
 
 const app = express();
+// db connection
+dbConnection();
 
 // expose public directory
 app.use(express.static("public"));
