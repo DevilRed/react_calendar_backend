f1d4ecc950c32ed51c255f719f59a4261b1b4231
Christian <chrisdaq@gmail.com>
2025-02-17 11:35:27 -0400

    validate if user exists

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,22 @@
 const User = require("../models/User");
 
 const userAdd = async (req, res) => {
+  const { email, password } = req.body;
   try {
-    // get params from req.body
-    const user = new User(req.body);
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        ok: false,
+        msg: "User already exists",
+      });
+    }
+    user = new User(req.body);
     await user.save();
 
     res.status(201).json({
       ok: true,
+      uid: user.id,
+      name: user.name,
     });
   } catch (error) {
     console.log(error);
