5ddc38d251a00e6a7d6ccdd34dce38f7a691fac0
Christian <chrisdaq@gmail.com>
2025-02-17 11:56:39 -0400

    logic for user login user

--- controllers/auth.js
+++ controllers/auth.js
@@ -30,12 +30,36 @@ const userAdd = async (req, res) => {
   }
 };
 
-const userLogin = (req, res) => {
+const userLogin = async (req, res) => {
   const { email, password } = req.body;
-
-  res.json({
-    ok: true,
-  });
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        ok: false,
+        msg: "User does not exist",
+      });
+    }
+    const validPassword = bcrypt.compareSync(password, user.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Invalid password",
+      });
+    }
+    // generate JWT
+    res.json({
+      ok: true,
+      uid: user.id,
+      name: user.name,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Please talk with administrator",
+    });
+  }
 };
 
 const tokenRenew = (req, res) => {
