6effec5c77c149af26e303993f7d705ebf0b5907
Christian <chrisdaq@gmail.com>
2025-02-21 10:14:47 -0400

    update event

--- controllers/events.js
+++ controllers/events.js
@@ -28,11 +28,42 @@ const addEvent = async (req, res) => {
   }
 };
 
-const updateEvent = (req, res) => {
-  res.json({
-    ok: true,
-    msg: "updateEvent",
-  });
+const updateEvent = async (req, res) => {
+  const eventId = req.params.id;
+  try {
+    const event = await Event.findById(eventId);
+    if (!event) {
+      return res.status(404).json({
+        ok: false,
+        msg: "Event not found",
+      });
+    }
+    // check event owner
+    //  event.user is an objectId, use toString() to compare it with the req.uid
+    if (event.user.toString() !== req.uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "You don't have the privilege to edit this event",
+      });
+    }
+    const newEvent = {
+      ...req.body,
+      user: req.uid,
+    };
+    const updatedEvent = await Event.findByIdAndUpdate(eventId, newEvent, {
+      new: true,
+    }); // new true to return the updated event
+    res.json({
+      ok: true,
+      event: updatedEvent,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Please contact the administrator",
+    });
+  }
 };
 
 const deleteEvent = (req, res) => {
